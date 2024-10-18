ccb148ada647c182c911485c98451d8c1bdd65cc
NowyAlexx <68709141+NowyAlexx@users.noreply.github.com>
2024-10-18 11:51:22 +0200

    Add files via upload

---
+++ u/config.js
@@ -0,0 +1,8 @@
+const config = {
+    // GitHub configuration
+    githubToken: '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', // Replace with your GitHub token
+    githubUsername: 'NowyAlexx', // Replace with your GitHub username
+    githubRepo: 'nowyalexx.github.io', // Replace with your GitHub repository name
+};
+
+export default config;
